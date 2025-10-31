b8d938329bb5d09e63c573181d9cc12415ca13bd
Chaitnya27 <chaitnyakhedekar02@gmail.com>
2025-10-31 23:00:05 +0530

    changes

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 export default function Hero() {
   return (
     <div className=' w-full'>
     <div className='flex flex-col justify-center items-center bg-blue-100 min-h-screen py-20 text-center'>
       <h1 className='text-green-500 mb-3 text-2xl font-bold'>NxtRole.AI Career Guidance Roadmap Generator</h1>
       <p className='mb-4 text-lg '>AI-powered platform that helps students and professionals find the right career path, discover job roles, and learn skills to grow.</p>
+      <Link to='/features'>
       <button className='bg-blue-600 rounded-md shadow-md text-white px-5 py-3 hover:bg-blue-800 hover:scale-105 transition-transform duration-300'>Explore Now</button>
-    </div>
+    
+      </Link>
+      </div>
+      
     </div>
   )
 }
